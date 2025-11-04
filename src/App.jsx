079ccccc13aa0c4d2b0659ac8eaaedfb1749e393
kalikranthivarma <kali.kv.kali@gmail.com>
2025-11-04 12:53:21 +0530

    kali-app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
-import React from 'react'
-
+import React from "react";
+import MovieList from "./components/MovieList";
 export default function App() {
   return (
     <div>
-      hi
+      <MovieList />
     </div>
-  )
+  );
 }
